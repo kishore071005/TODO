41c643073ad6dd2e2ee5e528d5de12b79142c858
Nanda Kishore <kishore071005@gmail.com>
2025-09-30 14:54:47 +0530

    Fix database configuration for Vercel

--- TODO/settings.py
+++ TODO/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'TODO.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'appdata',
-        'USER': 'postgres',
-        'PASSWORD': '2410',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DATABASE', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
